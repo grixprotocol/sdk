81f8427b8c97cb763e31413f68582a77b1efd185
asalef10 <asalef10@gmail.com>
2024-11-27 12:14:56 +0200

    Add method to sdk

--- index.d.ts
+++ index.d.ts
@@ -1,5 +1,18 @@
-// src/types/adapters.d.ts
-
 declare module "@grixprotocol/sdk" {
-	export function GrixSDK();
+	// Configuration type for the SDK
+	export type GrixConfig = {};
+
+	// GrixSDK class declaration
+	export class GrixSDK {
+		// Private constructor to prevent direct instantiation
+		private constructor(config: GrixConfig);
+
+		// Static method to initialize the SDK
+		static initialize(config: GrixConfig): Promise<GrixSDK>;
+
+		// Method to get chatbot context
+		fetchAssetPrice(asset: string): Promise<number>;
+		chatBotGetContext(): Promise<string>;
+		chatBotGetContext(userMessage: string, tradeboardOverride?: any): Promise<string>;
+	}
 }
--- index.test.ts
+++ index.test.ts
@@ -1,12 +1,36 @@
 import { GrixSDK } from "./src/index";
+ describe("GrixSDK Initialization and Chatbot Interactions", () => {
+	let sdk: GrixSDK;
+	let timeOut = 50000;
+	const OPENAI_API_KEY = process.env.OPENAI_API_KEY || ''; // Set this in your AWS Lambda environment variables.
 
-describe("GrixSDK Initialization and Chatbot Context Retrieval", () => {
-	// Initialize the SDK using the static method
 
-	test("Initialize GrixSDK and retrieve chatbot context", async () => {
-		const sdk = await GrixSDK.initialize({});
+	// Initialize the SDK once for all tests
+	beforeAll(async () => {
+		sdk = await GrixSDK.initialize({ openAIKey: OPENAI_API_KEY });
+	});
 
-		const chatbotMessage = await sdk.chatBotGetContext();
-		console.log("chatbotMessage", chatbotMessage);
-	}, 50000);
+ 	test(
+		"Retrieve chatbot response for a user message",
+		async () => {
+			// Mock user input
+			const userMessage = "What is the best offer for an option I can buy ?";
+
+			// Retrieve chatbot context and enhanced user message
+			const { systemInstructions, chatbotContext, enhancedUserMessage } =
+				await sdk.chatBotGetContext(userMessage);
+
+			// Send chatbot request
+			const response = await sdk.sendChatbotRequest({
+				chatbotContext,
+				enhancedUserMessage,
+				systemInstructions,
+				userContext: [],
+			});
+
+			// Log the chatbot response for debugging
+			console.log("Chatbot Response:", response);
+		},
+		timeOut
+	);
 });
