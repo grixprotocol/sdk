ef826f375e58ffcd121bdc76b9fdd67ce65fe139
Tomelia1999 <tom.elia99@gmail.com>
2025-03-24 10:36:14 +0200

    adding methods

---
+++ src/methods/perps/index.ts
@@ -0,0 +1,6 @@
+export * from './getPairs/index.js';
+export * from './getPredictedFundingRate/index.js';
+export * from './getAssetContexts/index.js';
+export * from './getAssetContexts/index.js';
+export * from './getHistoricalFundingRates/index.js';
+export * from './getOpenInterestCaps/index.js';
